57763acf6e7671c731120c805713d0e3442a3a87
John Matson <47843491+johnmatson@users.noreply.github.com>
2020-03-11 21:42:00 -0700

    v0.2
    lowpass filter buffer working

--- filters/verilog-implementation/lowpass.sv
+++ filters/verilog-implementation/lowpass.sv
@@ -1,14 +1,21 @@
 module lowpass #( parameter L = 5 ) (
                 input logic clk,
                 input logic [15:0] lowpassIn,
+                output logic [15:0] out4,out3,out2,out1,out0, // for testing only
                 output logic [15:0] lowpassOut);
 
-    logic [L-1:0] data [15:0];
+    logic [15:0] data [L-1:0];
 
     always_ff @ (posedge clk) begin
         for (int i = 0; i < L-1; i++)
             data[i] <= data[i+1];
         data[L-1] <= lowpassIn;
+
+        out4<=data[4];
+        out3<=data[3];
+        out2<=data[2];
+        out1<=data[1];
+        out0<=data[0];
     end
 
 endmodule
