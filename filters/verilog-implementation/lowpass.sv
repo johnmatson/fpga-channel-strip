49d3a6e9f8f2a71292ddcead60ac20d235cf9ac0
John Matson <47843491+johnmatson@users.noreply.github.com>
2020-03-23 23:45:46 -0700

    v1.7
    lowpass.sv & lowpass_tb.sv
    fixed errors in testbench sine wave
    changed all module internal vectors to 64 bit

--- filters/verilog-implementation/lowpass.sv
+++ filters/verilog-implementation/lowpass.sv
@@ -9,21 +9,21 @@ module lowpass #( parameter L = 3 ) (
     logic [15:0] data [L-1:0];
 
     // coefficients
-    logic [31:0] y1_coeff = 1.982*1024;
-    logic [31:0] y2_coeff = -0.982*1024;
-    logic [31:0] x0_coeff = 1*0.991*1024;
-    logic [31:0] x1_coeff = 2*0.991*1024;
-    logic [31:0] x2_coeff = 1*0.991*1024;
+    logic [63:0] y1_coeff = 1.982*1024;
+    logic [63:0] y2_coeff = -0.982*1024;
+    logic [63:0] x0_coeff = 1*0.991*1024;
+    logic [63:0] x1_coeff = 2*0.991*1024;
+    logic [63:0] x2_coeff = 1*0.991*1024;
 
     // buffer
-    logic [31:0] y1;
-    logic [31:0] y2;
-    logic [31:0] x0;
-    logic [31:0] x1;
-    logic [31:0] x2;
+    logic [63:0] y1;
+    logic [63:0] y2;
+    logic [63:0] x0;
+    logic [63:0] x1;
+    logic [63:0] x2;
 
     // temp output
-    logic [31:0] yn;
+    logic [63:0] yn;
 
     always_ff @ (posedge clk, negedge reset_n) begin
 
@@ -45,7 +45,7 @@ module lowpass #( parameter L = 3 ) (
             // data[L-1] <= lowpassIn;
 
             // 16 to 32 bit transfer with sign preservation
-            x0 <= { {16{lowpassIn[15]}}, lowpassIn };
+            x0 <= { {47{lowpassIn[15]}}, lowpassIn };
 
             // differnce equation
             yn <= y1_coeff*y1 + y2_coeff*y2 + x0_coeff*x0 + x1_coeff*x1 + x2_coeff*x2;
